a1e631b272502235b83e6bf5760f9e51ba92f4c2
Ramonasgit <ramona-b@live.at>
2023-09-08 10:53:03 +0200

    Add: WebSecurityConfig file for authorized Login

---
+++ webshop/src/main/java/com/fairgoods/webshop/security/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.fairgoods.webshop.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .securityMatcher("/**")
+                .authorizeHttpRequests(registry -> registry
+                        .requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
