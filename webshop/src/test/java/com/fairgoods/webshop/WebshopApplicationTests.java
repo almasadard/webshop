50220407e0fc4e83a292d8d9640ab2230f11079a
almasadard <122538843+almasadard@users.noreply.github.com>
2023-09-22 11:14:32 +0200

    added test

--- webshop/src/test/java/com/fairgoods/webshop/WebshopApplicationTests.java
+++ webshop/src/test/java/com/fairgoods/webshop/WebshopApplicationTests.java
@@ -2,8 +2,10 @@ package com.fairgoods.webshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("/application-test.properties")
 class WebshopApplicationTests {
 
 	@Test
