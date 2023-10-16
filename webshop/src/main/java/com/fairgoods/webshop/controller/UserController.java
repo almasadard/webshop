bb5867df03fb5ebc5784fa1bda46f1399e0d2b6e
Ramonasgit <ramona-b@live.at>
2023-10-16 13:38:31 +0200

    Delete: unused import

--- webshop/src/main/java/com/fairgoods/webshop/controller/UserController.java
+++ webshop/src/main/java/com/fairgoods/webshop/controller/UserController.java
@@ -6,7 +6,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
