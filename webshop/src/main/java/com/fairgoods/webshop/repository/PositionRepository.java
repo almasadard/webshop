1f91401d748932825499823c43aaebdfb01ec31a
Ramonasgit <ramona-b@live.at>
2023-10-20 15:09:36 +0200

    Delete: Unused Files

--- webshop/src/main/java/com/fairgoods/webshop/repository/PositionRepository.java
+++
@@ -1,9 +0,0 @@
-/*
-package com.fairgoods.webshop.repository;
-
-import com.fairgoods.webshop.model.Position;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PositionRepository extends JpaRepository<Position, Long> {
-}
-*/
